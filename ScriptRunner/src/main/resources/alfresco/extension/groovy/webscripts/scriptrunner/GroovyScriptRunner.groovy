5aec8fadc014a21ff5f953097869cfa0229d98c7
Peter Monks <pmonks@gmail.com>
2013-01-17 16:24:10 -0800

    Updated to v1.2. Upgraded POMs to latest and greatest artifacts.alfresco.com. Added Spring application context as a root scope variable.  Dangerous, but useful!

--- ScriptRunner/src/main/resources/alfresco/extension/groovy/webscripts/scriptrunner/GroovyScriptRunner.groovy
+++ ScriptRunner/src/main/resources/alfresco/extension/groovy/webscripts/scriptrunner/GroovyScriptRunner.groovy
@@ -5,6 +5,8 @@ import groovy.lang.GroovyShell
 
 import net.sf.json.*
 
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
 import org.springframework.extensions.webscripts.WebScriptRequest
 import org.springframework.extensions.webscripts.Status
 import org.springframework.extensions.webscripts.Cache
@@ -13,10 +15,17 @@ import org.alfresco.extension.webscripts.groovy.GroovyDeclarativeWebScript
 
 
 public class GroovyScriptRunner
-    implements GroovyDeclarativeWebScript
+    implements GroovyDeclarativeWebScript,
+               ApplicationContextAware
 {
-    def serviceRegistry
+    private def applicationContext
+    private def serviceRegistry
 
+    public void setApplicationContext(ApplicationContext applicationContext)
+    {
+        this.applicationContext = applicationContext
+        this.serviceRegistry    = applicationContext.getBean("ServiceRegistry")
+    }
 
     public Map<String, Object> execute(WebScriptRequest request, Status status, Cache cache)
     {
@@ -25,7 +34,8 @@ public class GroovyScriptRunner
 
         if (script)
         {
-            def binding      = new Binding([serviceRegistry:serviceRegistry])
+            def binding      = new Binding([serviceRegistry:serviceRegistry,
+                                            applicationContext:applicationContext])
             def scriptOutput = new GroovyShell(this.getClass().getClassLoader(), binding).evaluate(script)
 
             if (request.getFormat() == "json")
